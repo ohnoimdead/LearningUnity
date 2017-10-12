92f0829798ef402582f9209796e873418f92549b
Tres Henry <tres@treshenry.net>
2017-10-12 16:47:58 -0700

    Now with missing files

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+    [HideInInspector]
+    public int health;
+
+    public void Start() {
+        switch (gameObject.name) {
+            case SceneController.SHELL_CLONE:
+                health = SceneController.SHELL_COST;
+                break;
+            case SceneController.PLATFORM_CLONE:
+                health = SceneController.PLATFORM_COST;
+                break;
+        }
+    }
+
+    // Called by the watcher when it sees this object
+    public bool ISeeYou() {
+        // If we see the shell the player currently posesses, zap the player
+        if (gameObject.name == SceneController.SHELL_CLONE && gameObject.GetComponent<ShellController>().posessed) {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().ISeeYou();
+            return false;
+        } else {
+            health -= 1;
+
+            if (health <= 0) {
+                GameObject.Destroy(gameObject);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
