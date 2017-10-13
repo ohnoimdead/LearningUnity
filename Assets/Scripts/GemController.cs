dd81fd8da49a9bd7ffc39192e8df15e5bbc6ea77
Tres Henry <tres@treshenry.net>
2017-10-12 17:04:39 -0700

    Gems now rotate at random speed

---
+++ Assets/Scripts/GemController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GemController : MonoBehaviour {
+    public float rotationSpeedMin = 10.0f;
+    public float rotationSpeedMax = 20.0f;
+
+    private float rotationSpeed;
+
+    public void Start () {
+        // pick a speed
+        rotationSpeed = Random.Range(rotationSpeedMin, rotationSpeedMax);
+    }
+
+    public void Update() {
+        gameObject.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
